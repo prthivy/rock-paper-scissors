a8a252253c13bdcd2114dc119fd336b965f550d5
prthivy <prithviisinamood@gmail.com>
2025-05-12 19:24:47 +0530

    write potential logic for choice conversion

--- repos/rock-paper-scissors/rock-paper-scissors.js
+++ repos/rock-paper-scissors/rock-paper-scissors.js
@@ -11,4 +11,14 @@ getComChoice(); //rng returns
 usr = prompt("choose between rock, paper and scissors!") //user choice
 console.log(usr);
 
+if(usr == rock) {
+    usr = 0;
+}
+elif (usr == paper) {
+    usr = 1;
 
+}
+
+elif (usr == scissors) {
+    usr = 2; 
+}

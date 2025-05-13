3af7266d79b6238343a6a1a9e12c06b2ee2bd491
prthivy <prithviisinamood@gmail.com>
2025-05-13 11:36:44 +0530

    modify choice logic

--- repos/rock-paper-scissors/rock-paper-scissors.js
+++ repos/rock-paper-scissors/rock-paper-scissors.js
@@ -1,24 +1,24 @@
-function getComChoice(com) { 
-    com = (Math.random() * 3); //rng [0,3)
-    return Math.floor(com); //deci -> int return
+function getComputerChoice(computer) { 
+    computer = (Math.random() * 3); //rng [0,3)
+    return Math.floor(computer); //deci -> int return
 }
 
-getComChoice(); //rng returns
+let actualComputerChoice = getComputerChoice(); //rng returns
 
+function getHumanChoice(human) {
+    human = prompt("choose between rock, paper and scissors!"); //user choice
+    //convert usr choice to number for comparison with com choice
+    if(human == "rock") {
+        human = 0;
+    }
+    else if (hum == "paper") {
+        human = 1;
+    }
 
-let usr;
-usr = prompt("choose between rock, paper and scissors!") //user choice
-console.log(usr);
-
-if(usr == "rock") {
-    usr = 0;
+    else if (human == "scissors") {
+        human = 2; 
+    }
+    return human; 
 }
-else if (usr == "paper") {
-    usr = 1;
 
-}
-
-else if (usr == "scissors") {
-    usr = 2; 
-}
-console.log(usr);
+let actualHumanChoice = getHumanChoice();

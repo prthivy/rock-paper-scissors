7064ffd774d55dba13c98c86d873b003ad64dd68
prthivy <prithviisinamood@gmail.com>
2025-05-13 12:25:12 +0530

    add gameplay logic for single round

--- repos/rock-paper-scissors/rock-paper-scissors.js
+++ repos/rock-paper-scissors/rock-paper-scissors.js
@@ -1,17 +1,21 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(computer) { 
     computer = (Math.random() * 3); //rng [0,3)
     return Math.floor(computer); //deci -> int return
 }
 
-let actualComputerChoice = getComputerChoice(); //store rng choice (num)
+let computerChoice = getComputerChoice(); //store rng choice (num)
 
 function getHumanChoice(human) {
     human = prompt("choose between rock, paper and scissors!"); //human choice
+    human = human.toLowerCase(); //account for inconsistent casing
     //convert human choice to numb for comp with computer choice
     if(human == "rock") {
         human = 0;
     }
-    else if (hum == "paper") {
+    else if (human == "paper") {
         human = 1;
     }
 
@@ -21,6 +25,22 @@ function getHumanChoice(human) {
     return human; 
 }
 
-let actualHumanChoice = getHumanChoice(); //store human choice (num)
+let humanChoice = getHumanChoice(); //store human choice (num)
 
-actualComputerChoice == actualHumanChoice; //compare
+function playRound(humanChoice, computerChoice) { //scoring logic
+    if (humanChoice > computerChoice) {
+        console.log("yew win!");
+        humanScore =+ 1;
+    }
+    else if (computerChoice > humanChoice) {
+        console.log("yew lews..");
+        computerScore =+ 1;
+    }
+    else if (computerChoice == humanChoice) {
+        console.log("thai!"); 
+    }
+}
+
+playRound(humanChoice, computerChoice);
+console.log(`human score is ${humanScore} points`);
+console.log(`computer score is ${computerScore} points`);

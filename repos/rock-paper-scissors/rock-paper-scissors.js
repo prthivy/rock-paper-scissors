238b28fed106d7e4e9636781337b67bb10796a81
prthivy <prithviisinamood@gmail.com>
2025-05-14 14:32:59 +0530

    overhaul entire code to implement game logic

--- repos/rock-paper-scissors/rock-paper-scissors.js
+++ repos/rock-paper-scissors/rock-paper-scissors.js
@@ -1,46 +1,91 @@
 let humanScore = 0;
 let computerScore = 0;
+let round_no = 1;
 
-function getComputerChoice(computer) { 
-    computer = (Math.random() * 3); //rng [0,3)
-    return Math.floor(computer); //deci -> int return
+function getComputerChoice() { 
+    let computerChoice = (Math.random() * 3); //rng [0,3)
+    computerChoice = Math.floor(computerChoice); //deci -> int return
+    if (computerChoice == 0) {
+        computerChoice = "rock";
+    }
+    
+    else if (computerChoice == 1) {
+        computerChoice = "paper";
+    }
+
+    else if (computerChoice == 2) {
+        computerChoice = "scissors"
+    }
+    return computerChoice;
 }
 
-let computerChoice = getComputerChoice(); //store rng choice (num)
-
-function getHumanChoice(human) {
-    human = prompt("choose between rock, paper and scissors!"); //human choice
-    human = human.toLowerCase(); //account for inconsistent casing
-    //convert human choice to numb for comp with computer choice
-    if(human == "rock") {
-        human = 0;
-    }
-    else if (human == "paper") {
-        human = 1;
-    }
-
-    else if (human == "scissors") {
-        human = 2; 
-    }
-    return human; 
+function getHumanChoice() {
+    let humanChoice = prompt("choose between rock, paper and scissors!"); //gets human choice
+    humanChoice = humanChoice.toLowerCase(); //account for inconsistent casing
+    return humanChoice; 
 }
 
-let humanChoice = getHumanChoice(); //store human choice (num)
+//scoring logic
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log(`computer chose: ${computerChoice}`);
+        console.log("round won!");
+        humanScore += 1;
+        console.log(`you: ${humanScore}, computer: ${computerScore}.`);
 
-function playRound(humanChoice, computerChoice) { //scoring logic
-    if (humanChoice > computerChoice) {
-        console.log("yew win!");
-        humanScore =+ 1;
     }
-    else if (computerChoice > humanChoice) {
-        console.log("yew lews..");
-        computerScore =+ 1;
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log(`computer chose: ${computerChoice}`);
+        console.log("round lost..");
+        computerScore += 1;
+        console.log(`you: ${humanScore}, computer: ${computerScore}.`);
     }
+
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log(`computer chose: ${computerChoice}`);
+        console.log("round won!");
+        humanScore += 1;
+        console.log(`you: ${humanScore}, computer: ${computerScore}.`);
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log(`computer chose: ${computerChoice}`);
+        console.log("round lost..");
+        computerScore += 1;
+        console.log(`you: ${humanScore}, computer: ${computerScore}.`);
+    }
+
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log(`computer chose: ${computerChoice}`);
+        console.log("round won!");
+        humanScore += 1;
+        console.log(`you: ${humanScore}, computer: ${computerScore}.`);
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log(`computer chose: ${computerChoice}`);
+        console.log("round lost..");
+        computerScore += 1;
+        console.log(`you: ${humanScore}, computer: ${computerScore}.`);
+    }
+
     else if (computerChoice == humanChoice) {
-        console.log("thai!"); 
+        console.log(`computer chose: ${computerChoice}`);
+        console.log("round tied."); 
+        console.log(`you: ${humanScore}, computer: ${computerScore}.`);
     }
 }
 
-playRound(humanChoice, computerChoice);
-console.log(`human score is ${humanScore} points`);
-console.log(`computer score is ${computerScore} points`);
+while (humanScore < 3 && computerScore < 3) {  //calls the playRound function until either score reaches 3
+    console.log(`round ${round_no}: `)
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    let playGame = playRound(humanChoice, computerChoice);
+    round_no += 1; 
+}
+
+//displays winner message
+if (humanScore == 3) {
+    console.log("you win the game!")
+}
+else if (computerScore == 3) {
+    console.log("computer wins the game!")
+}

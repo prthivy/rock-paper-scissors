50db651f6b0a092716a054c16aeefcf41c70a1a0
prthivy <prithviisinamood@gmail.com>
2025-05-14 15:22:12 +0530

    add comments explaining the flow

--- repos/rock-paper-scissors/rock-paper-scissors.js
+++ repos/rock-paper-scissors/rock-paper-scissors.js
@@ -76,6 +76,7 @@ function playRound(humanChoice, computerChoice) {
 
 while (humanScore < 3 && computerScore < 3) {  //calls the playRound function until either score reaches 3
     console.log(`round ${round_no}: `)
+
     let computerChoice = getComputerChoice();
     let humanChoice = getHumanChoice();
     let playGame = playRound(humanChoice, computerChoice);
@@ -89,3 +90,38 @@ if (humanScore == 3) {
 else if (computerScore == 3) {
     console.log("computer wins the game!")
 }
+
+//personal walkthrough
+
+//1. logic to get computer choice:
+    // 1.1 a function which when called, creates a random choice between rock paper scissor
+    // 1.2 a variable to hold the rng logic
+    // 1.3 assign rng to the variable  between 0 and 1
+    // 1.4 multiply this with 3 to get random number between 0 and 3
+    // 1.5 floor this to get 0, 1 or 2
+    // 1.6 assign the 3 numbers to 3 choices
+    // 1.7 specify return value as the variable
+    // 1.x when this function is called, it'll return rock paper or scissor
+
+//2. logic to get human choice:
+    // 2.1 a function which when called, asks user for rock, paper or scissors
+    // 2.2 a variable to hold this string
+    // 2.3 modify string to only be lowercase, to resolve inconsistent casing
+    // 2.4 specify return value as the variable
+    // 2.x when this function is called, it'll return user choice in lowercase
+
+//3. logic for scoring the game:
+    // 3.1 a function which when called, will do string comparisons to see who won
+    // 3.2 this function will have two parameters, each being a function reference (callback function) to the computer and human choice functions
+    // 3.3 when this function is called, it'll call both human and computer choice functions and get their return values as human and computer choices
+    // 3.4 this function will then compare both return values and decide who wins
+    // 3.5 this function will then increase the winner's score by 1, and will therefore need two global variables for human and computer scores resp. for ties it'll do nothing.
+    // 3.x this function has no return value
+    
+//4. logic for playing multiple rounds:
+    // 4.1 the game needs to be played until either score reaches 3 points. 
+    // 4.2 so we need a while loop which will keep calling scoring function with callback functions to human and computer choice as parameters.
+    // 4.x this function runs until either score reaches 3
+
+//5. simple conditional that checks which out of either scores is 3 and declares it the winner
+
